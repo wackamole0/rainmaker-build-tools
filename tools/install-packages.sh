bffabe4494b78ec21ea5845b881e2b739813cf26
Adam Dowling <adamd@deeson.co.uk>
2015-01-22 19:46:42 +0000

    - Setup basic repo structure and created script to install packages

---
+++ tools/install-packages.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt-get install -y \
+	apache2-dev \
+	bridge-utils \
+	dnsutils \
+	fuse \
+	jwhois \
+	git \
+	htop \
+	iftop \
+	iotop \
+	iptables \
+	iptraf \
+	lftp \
+	lsof \
+	lsyncd \
+	lxc \
+	man \
+	mtr \
+	mytop \
+	nano \
+	nmap \
+	ntop \
+	ntp \
+	openssh-client \
+	openssh-server \
+	pv \
+	rssh \
+	rsync \
+	siege \
+	sshfs \
+	sysstat \
+	tcpdump \
+	telnet \
+	traceroute \
+	wget
