0758cc1a21b457ccb0341ed2817ca1e3bfbaf068
Adam Dowling <adamd@deeson.co.uk>
2015-02-10 18:28:54 +0000

    - Updated script used to customize official Ubuntu 14.04 Vagrant box

--- build/tools/prepare-vagrant-box.sh
+++ build/tools/prepare-vagrant-box.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 #
+# This script is used to customise the official Ubuntu 14.04 Vagrant box provided Canonical (URL below).
+#
+# https://vagrantcloud.com/ubuntu/boxes/trusty64
+#
+
+#
 # Start by upgrading to latest version of 14.04
 #
 apt-get -y update
@@ -32,3 +38,10 @@ apt-get -y update
 apt-get -y install puppet
 unlink /tmp/puppetlabs-release-trusty.deb
 
+#
+# Cleanup
+#
+apt-get clean
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+cat /dev/null > ~/.bash_history && history -c
