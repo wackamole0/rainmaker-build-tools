6a207ad4ab5ad394af68bf7463a469ff0ca8292a
Adam Dowling <adamd@deeson.co.uk>
2015-12-10 12:44:35 +0000

    - Install packages which can be optionally required by Salt

--- tools2/common/bootstrap-salt.sh
+++ tools2/common/bootstrap-salt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y debconf-utils python-software-properties software-properties-common git
+apt-get install -y debconf-utils python-software-properties software-properties-common git pciutils dmidecode
 
 add-apt-repository -y ppa:saltstack/salt
 apt-get update -y
