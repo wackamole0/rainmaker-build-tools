79b01d3dba24a653d319753972aea21bd19906da
Adam Dowling <adamd@deeson.co.uk>
2015-12-15 11:10:19 +0000

    - Updated project container build tools to /var/cache/lxc from the root VM into the container.

--- tools2/project/mount-filesystems.sh
+++ tools2/project/mount-filesystems.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-if [ `id -u` -ne 0 ]
-then
-  echo "You must run this with root permissions"
-  exit
+if [ `id -u` -ne 0 ]; then
+    echo "You must run this with root permissions"
+    exit
 fi
 
 script_path=`dirname $0`
 
 # Check we have been given a name for the container we are going to build
 
-if [ "$1" == "" ]
-then
-  echo "A name for the container must be specified"
-  exit
+if [ "$1" == "" ]; then
+    echo "A name for the container must be specified"
+    exit
 fi
 
 container_lxc_name=$1
@@ -22,18 +20,24 @@ container_lxc_root_fs="$container_lxc_root/rootfs"
 
 # Mount /mnt/tools from root VM into container
 
-if [ ! -d "$container_lxc_root_fs/mnt/tools" ]
-then
-  mkdir "$container_lxc_root_fs/mnt/tools"
+if [ ! -d "$container_lxc_root_fs/mnt/tools" ]; then
+    mkdir "$container_lxc_root_fs/mnt/tools"
 fi
 
 mount -o bind /mnt/rainmaker-tools "$container_lxc_root_fs/mnt/tools"
 
 # Mount /srv/saltstack from root VM into container
 
-if [ ! -d "$container_lxc_root_fs/srv/saltstack" ]
-then
-  mkdir "$container_lxc_root_fs/srv/saltstack"
+if [ ! -d "$container_lxc_root_fs/srv/saltstack" ]; then
+    mkdir "$container_lxc_root_fs/srv/saltstack"
 fi
 
 mount -o bind /srv/saltstack "$container_lxc_root_fs/srv/saltstack"
+
+# Mount /var/cache/lxc from root VM into container
+
+if [ ! -d "$container_lxc_root_fs/var/cache/lxc" ]; then
+    mkdir --mode 0700 "$container_lxc_root_fs/var/cache/lxc"
+fi
+
+mount -o bind /var/cache/lxc "$container_lxc_root_fs/var/cache/lxc"
