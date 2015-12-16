4cd689092fa4b1c5601d4352113c28dd971a739b
Adam Dowling <adamd@deeson.co.uk>
2015-12-16 13:27:15 +0000

    - Added script to install the Rainmaker CLI

---
+++ tools/common/install-rainmaker-cli.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+script_path=`dirname $0`
+tools_path="$script_path/.."
+
+if [ ! -d /opt/rainmaker-cli ]; then
+    mkdir -p /opt/rainmaker-cli
+fi
+
+curdir=`pwd`
+
+apt-get install -y php5-curl
+
+cd /opt/rainmaker-cli
+git clone https://github.com/wackamole0/rainmaker-tool.git .
+git checkout v0.1
+
+cd $curdir
+
+if [ ! -h /usr/local/bin/rainmaker ]; then
+    ln -s /opt/rainmaker-cli/rainmaker /usr/local/bin/rainmaker
+fi
