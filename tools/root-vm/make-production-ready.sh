c5be3e5bfcba38d0a0b8f6f756b74556d4cf12c2
Adam Dowling <adam.dowling@gmail.com>
2015-12-19 11:22:51 +0000

    - Updated tool which makes a Rainmaker box production ready so that all but the base Salt environment is removed and so the LXC caches are emptied

--- tools/root-vm/make-production-ready.sh
+++ tools/root-vm/make-production-ready.sh
@@ -11,13 +11,24 @@ tools_path="$script_path/.."
 cp $script_path/config/default/nic-br0.cfg /etc/network/interfaces.d/br0.cfg
 cp $script_path/config/default/nfs-exports /etc/exports
 
-if [ -d /srv/saltstack/salt/builder ]; then
-    rm -Rf /srv/saltstack/salt/builder
-fi
+saltenvs="builder profile-builder testbed"
 
-if [ -d /srv/saltstack/pillar/builder ]; then
-    rm -Rf /srv/saltstack/pillar/builder
-fi
+for saltenv in $saltenvs
+do
+    rprofmgr node:remove --salt-environment=$saltenv rainmaker.localdev
+    rprofmgr node:remove --salt-environment=$saltenv services.rainmaker.localdev
+
+    if [ -d "/srv/saltstack/salt/$saltenv" ]; then
+        rm -Rf "/srv/saltstack/salt/$saltenv"
+    fi
+
+    if [ -d "/srv/saltstack/pillar/$saltenv" ]; then
+        rm -Rf "/srv/saltstack/pillar/$saltenv"
+    fi
+done
+
+# Clear LXC caches
+find /var/cache/lxc/* -maxdepth 0 -type d | fgrep -v rainmaker | xargs rm -Rf
 
 apt-get clean
 dd if=/dev/zero of=/EMPTY bs=1M
